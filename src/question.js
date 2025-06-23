21121e65a926a04c11a6876415c6f3d432d26474
Raquel Lacorte <costa.lacorte@gmail.com>
2025-06-23 16:26:06 +0100

    First day

--- src/question.js
+++ src/question.js
@@ -1,7 +1,21 @@
 class Question {
+    constructor(text, choices, answer, difficulty) {
+        this.text = text;
+        this.choices = choices;
+        this.answer = answer;
+        this.difficulty = difficulty;
+    }
+    shuffleChoices() {
+        for(let i = this.choices.length - 1; i > 0; i-- ) {
+            const a = Math.floor(Math.random() * i +1);
+            [this.choices[i]], this.choices[a] = [this.choices[a], this.choices[i]];
+        }
+    }
+}
     // YOUR CODE HERE:
+
     //
     // 1. constructor (text, choices, answer, difficulty)
+    
 
     // 2. shuffleChoices()
-}
--- src/quiz.js
+++ src/quiz.js
@@ -1,5 +1,47 @@
-class Quiz {
-    // YOUR CODE HERE:
+class Quiz { 
+    constructor(questions, timeLimit, timeRemaining) {
+        this.questions = questions; 
+        this.timeLimit = timeLimit;
+        this.timeRemaining = timeRemaining;
+        this.correctAnswers = 0;
+        this.currentQuestionIndex = 0;
+    }
+
+    getQuestion() {
+    return this.questions[this.currentQuestionIndex];
+    }
+
+    moveToNextQuestion() {
+        this.currentQuestionIndex++;
+    } 
+
+    shuffleQuestions() {
+        for(let i = this.questions.length - 1; i > 0; i-- ) {
+            const a = Math.floor(Math.random() * i +1);
+            [this.questions[i]], this.questions[a] = [this.questions[a], this.questions[i]];
+        }
+    }
+
+    checkAnswer(answer) {
+        if (answer === this.questions[this.currentQuestionIndex].answer) {
+              this.correctAnswers++;
+        }
+            
+    }
+
+    hasEnded() {
+        if (this.currentQuestionIndex < this.questions.length) {
+            return false;
+        } else if (this.currentQuestionIndex === this.questions.length) {
+            return true;
+        }
+    
+
+
+    }
+}
+
+// YOUR CODE HERE:
     //
     // 1. constructor (questions, timeLimit, timeRemaining)
 
@@ -12,4 +54,3 @@ class Quiz {
     // 5. checkAnswer(answer)
 
     // 6. hasEnded()
-}
